e23971cad316a00784d9286f30283bfdd3fcb99b
MixologyXBot <146635284+MixologyXBot@users.noreply.github.com>
2025-01-31 09:56:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from bot import Bot
 import pyrogram.utils
+from pyrogram import utils as pyroutils
 
 # pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+
+pyroutils.MIN_CHAT_ID = -999999999999
+pyroutils.MIN_CHANNEL_ID = -100999999999999
 
 Bot().run()
